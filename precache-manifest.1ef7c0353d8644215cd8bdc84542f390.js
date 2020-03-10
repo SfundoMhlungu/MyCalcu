9694520b939a653a0f2687685d9f43529e6ef0e4
Sfundo Mhlungu <mhlungusk@gmail.com>
2020-03-10 21:18:09 +0200

    My Calculator

---
+++ precache-manifest.1ef7c0353d8644215cd8bdc84542f390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5172e6a7726ff63b950169a7c440c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16c2a0dae5754c0992a9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6446cdb94f194bc5c210",
+    "url": "/static/js/2.ea5a6837.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.ea5a6837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c2a0dae5754c0992a9",
+    "url": "/static/js/main.5080c18d.chunk.js"
+  },
+  {
+    "revision": "2c8039270614a39c8cea",
+    "url": "/static/js/runtime-main.44716643.js"
+  }
+]);
